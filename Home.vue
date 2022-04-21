88875fbdf6e8526dc3ef6eded676b7837953efbb
naresh-500apps <104010737+naresh-500apps@users.noreply.github.com>
2022-04-21 19:11:44 +0530

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>HelloWorld</h1>
+        <p>iam naresh</p>
+        <button type="submit" @click="res">Submit</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"HomE",
+    data(){
+        return{
+            name:"naresh",
+        }
+    },
+    methods:{
+        res(){
+            document.write(this.name)
+        }
+    }
+}
+</script>
+
+
---
+++ table.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>enter table format</h1>
+        <b-table  :items="details"></b-table>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:"TablE",
+    data(){
+        return{
+         details:[{sno:"1",name:"suri",email:"suri123@gmail.com",contact:"879456123",city:"kakinada"},
+                  {sno:"2",name:"ramu",email:"suri123@gmail.com",contact:"879456123",city:"gudivada"},
+                  {sno:"3",name:"sreenu",email:"suri123@gmail.com",contact:"879456123",city:"razole"},
+                  {sno:"4",name:"mahesh",email:"suri123@gmail.com",contact:"879456123",city:"vizag"}]
+        }
+    },
+}
+</script>
