6b53873c7fd1190be8e8d7e564528965270bf1aa
naresh-500apps <naresh.yerramsetty@mantragroup.com>
2022-04-25 19:45:51 +0530

    hii1

---
+++ 20/april20/22/04/2022/assignment-2/Q10.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button @click="fill()">click me</button>
+    </div>
+</template>
+<script>
+
+
+export default{
+    name:'VieW',
+    data(){
+        return{
+            officers: [ { id: 20, name: 'Captain Piett' }, 
+            { id: 24, name: 'General Veers' }, 
+            { id: 56, name: 'Admiral Ozzel' }, 
+            { id: 88, name: 'Commander Jerjerrod' } ],
+        }
+    },
+    methods:{
+        fill(){
+            var result = this.officers.map((x)=>{
+                return x.id
+            })
+            console.log(result)
+    },
+    }
+}
+</script>
