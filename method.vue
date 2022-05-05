25d25b3f790d98a82bfc8cd01695eb14ad3ac370
naresh-500apps <104010737+naresh-500apps@users.noreply.github.com>
2022-05-05 19:28:33 +0530

    Add files via upload

---
+++ method.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1 align="center">method and computed condition</h1>   
+        <b-button v-on:click="a++" variant="primary">add To A</b-button> &nbsp;
+        <b-button v-on:click="b++" variant="secondary">add To B</b-button>   
+        <p>A - {{ a }}</p>
+        <p>B - {{ b }}</p>
+        <p>Age + A = {{ addToA}}</p>
+        <p>Age + B = {{ addToB}}</p>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:'MEcd',
+    data(){
+        return{
+           Age:20,
+           a:0,
+           b:0
+        }
+        
+    },
+   // methods:{
+    //    addToA: function() {
+    //        console.log('addToA')
+    //          return this.a + this.Age;       
+            
+    //     },
+    
+    // addToB: function() {
+    //     console.log('addToB')
+    //         return this.b + this.Age;
+    // }
+computed:{
+     addToA: function() {
+         console.log('addToA')
+             return this.a + this.Age;                        
+} , 
+    addToB: function() {
+        console.log('addToB')
+            return this.b + this.Age;
+}
+}
+}
+
+</script>
